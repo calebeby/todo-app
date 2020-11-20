1fdff11b5a986b56cb5ebf93242b0fa7d913758b
Caleb Eby <caleb.eby01@gmail.com>
2020-11-19 19:35:24 -0800

    Add dotenv

--- server/index.js
+++ server/index.js
@@ -1,25 +1,36 @@
-import Pg from "pg";
-import polka from "polka";
+import pg from 'pg'
+import polka from 'polka'
 import { sql } from 'sqliterally'
+import * as dotenv from 'dotenv'
 
-const app = polka();
+// Load environment variables from .env file
+dotenv.config()
+
+const app = polka()
 app.get('/', (req, res) => {
-    res.end('Hello World!');
-});
-
-app.listen(3000);
-
-const main = async() => {
-  const client = new Pg.Client({
-    user: "",
-    password: "",
-    database: "",
+  res.end('Hello World!')
 })
-await client.connect() 
-const res = await client.query(sql`
+
+app.listen(3000)
+
+const { DB_USER, DB_PASSWORD } = process.env
+
+if (!DB_USER) throw new Error('Database user is required')
+if (!DB_PASSWORD) throw new Error('Database user is required')
+
+const main = async () => {
+  const client = new pg.Client({
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: 'todo-app',
+  })
+  await client.connect()
+  const res = await client.query(sql`
 SELECT *
 FROM "table"
 `)
-console.log(res)
+  console.log(res.rows)
 }
-main();
+
+main()
+
