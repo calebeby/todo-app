a3f013a5f607c8d75a39769c1a1d493f7bb66aae
Cassandra Esvelt <cesvelt22@my.whitworth.edu>
2020-12-15 11:45:55 -0800

    drag and drop tasks

--- ui/src/list-view.tsx
+++ ui/src/list-view.tsx
@@ -1,6 +1,6 @@
 import { useSetLastView } from './home'
 import { useRequireLogin } from './login'
-import { updateTask, useAllLabels, useTasks } from './state'
+import { updateTask, useAllLabels, useTasks, setTaskLabels } from './state'
 import { TaskWithLabels } from './task'
 import { getColorBrightness } from './utilities'
 import { Tabs } from './tabs'
@@ -9,6 +9,7 @@ import { mdiPencil } from '@mdi/js'
 import { Icon } from './icon'
 import { createTaskPopup } from './create-task-popup'
 
+//What about the tasks with labels that are not columns? they need to go into the unlabeled category.
 const oneWeekAgo = new Date(new Date().getTime() - 7 * 24 * 60 * 60 * 1000)
 
 export const ListView = () => {
@@ -28,40 +29,77 @@ export const ListView = () => {
       </div>
       <div class="list-view-lists">
         <Column
-          key="unlabeled"
-          tasks={tasks.filter((task) => task.labels.length === 0)}
+          id="unlabeled"
+          tasks={tasks.filter((task) =>
+            task.labels.every((label) =>
+              columns.every((col) => col.id !== label),
+            ),
+          )}
           name="Unlabeled"
         />
         {columns.map((column) => {
           return (
             <Column
               key={column.id}
-              labelId={column.id}
+              id={column.id}
               tasks={tasks.filter((task) => task.labels.includes(column.id))}
               name={column.name}
               color={column.color}
             />
           )
         })}
-        <Column key="done" tasks={doneTasksInLastWeek} name="Done" />
+        <Column id="done" tasks={doneTasksInLastWeek} name="Done" />
       </div>
     </div>
   )
 }
 
 const Column = ({
+  id,
   tasks,
   name,
   color,
-  labelId,
 }: {
+  id: 'unlabeled' | 'done' | number
   tasks: TaskWithLabels[]
   name: string
   color?: string
-  labelId?: number
 }) => {
   return (
-    <div class="list-view-list">
+    <div
+      class="list-view-list"
+      onDragOver={(e) => {
+        e.preventDefault()
+      }}
+      onDrop={(e) => {
+        if (e.dataTransfer === null) return
+        const stringdraggedTask = e.dataTransfer.getData('TaskWithLabels')
+        const oldColId = e.dataTransfer.getData('oldColId')
+
+        const draggedTask: TaskWithLabels = JSON.parse(stringdraggedTask)
+        e.preventDefault()
+        //dropping into the done column
+        if (id === 'done') updateTask({ is_done: true }, draggedTask.id)
+        //if leaving the done column and not moving to the unlabled column
+        //This is needed in order to make sure is_done is unchecked again
+        else if (id !== 'unlabeled' && oldColId === 'done') {
+          updateTask({ is_done: false }, draggedTask.id)
+          setTaskLabels(draggedTask.id, [...draggedTask.labels, id])
+        }
+        //dropping a task from either unlabled to normal column or normal column to normal column.
+        else if (id !== 'unlabeled' && oldColId !== 'done') {
+          //remove old label
+          let updatedLabels = draggedTask.labels.filter(
+            //Do we want the old label to be removed or no??
+            (labelId) => String(labelId) !== oldColId,
+          )
+          //add new label
+          setTaskLabels(draggedTask.id, [...updatedLabels, id])
+        } else if (id === 'unlabeled') {
+          setTaskLabels(draggedTask.id, [])
+        }
+      }}
+    >
       <h2
         style={{
           background: color,
@@ -69,8 +107,8 @@ const Column = ({
         }}
       >
         {name}
-        {labelId !== undefined && (
-          <button onClick={() => createLabelPopup(labelId)}>
+        {typeof id === 'number' && (
+          <button onClick={() => createLabelPopup(id)}>
             <Icon icon={mdiPencil} />
           </button>
         )}
@@ -81,7 +119,20 @@ const Column = ({
           .sort((a, b) => a.due_date - b.due_date)
           .map((task) => {
             return (
-              <li class="weekday-task" key={task.id}>
+              <li
+                class="weekday-task"
+                key={task.id}
+                draggable
+                onDragStart={(e) => {
+                  if (e.dataTransfer !== null) {
+                    e.dataTransfer.setData(
+                      'TaskWithLabels',
+                      JSON.stringify(task),
+                    )
+                    e.dataTransfer.setData('oldColId', String(id))
+                  }
+                }}
+              >
                 <input
                   type="checkbox"
                   checked={task.is_done}
@@ -90,7 +141,7 @@ const Column = ({
                     updateTask({ is_done: checked }, task.id)
                   }}
                 />
-                <a href={`/tasks/${task.id}`}>
+                <a href={`/tasks/${task.id}`} draggable={false}>
                   <span>{task.title}</span>
                   <span>
                     {task.due_date.toLocaleTimeString('en-US', {
